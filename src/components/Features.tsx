c8cd225ce3595a0c69717f17737fc12908c1b678
angelafeliciaa <angela_felicia@yahoo.com>
2024-03-24 01:34:19 -0700

    feat: cards

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -42,42 +42,32 @@ const features: FeatureProps[] = [
   },
 ];
 
-const featureList: string[] = [
-  "Dark/Light theme",
-  "Reviews",
-  "Features",
-  "Pricing",
-  "Contact form",
-  "Our team",
-  "Responsive design",
-  "Newsletter",
-  "Minimalist",
-];
+// document.querySelector('.controls').style.display = "none";
+
 
 export const Features = () => {
   return (
     <section id="features" className="container py-24 sm:py-32 space-y-8">
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-        Many{" "}
+        Problem{" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Great Features
+          Bank
         </span>
       </h2>
+      <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground text-center">
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis dolor pariatur sit!
+</p>
 
-      <div className="flex flex-wrap md:justify-center gap-4">
-        {featureList.map((feature: string) => (
-          <div key={feature}>
-            <Badge variant="secondary" className="text-sm">
-              {feature}
-            </Badge>
-          </div>
-        ))}
-      </div>
+
 
       <div className="grid grid-cols-3"> {/* Reduced gap to 2 */}
         {/* First Card */}
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img src={engineimg} alt="Engine" className="w-full h-auto" />
+
+        <div className="sketchfab-embed-wrapper"> 
+        <iframe title="3d Printable Pneumatic-Engined Toy Car" allowFullScreen  allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/2d297eed7b24451a8fb47a9372274778/embed?autospin=1&autostart=1" width="400" height="250"> </iframe></div>
+
+          {/* <img src={engineimg} alt="Engine" className="w-full h-auto" /> */}
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
             <p className="text-gray-700 text-base">
@@ -91,9 +81,15 @@ export const Features = () => {
           </div>
         </div>
 
+
         {/* Second Card */}
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img src={engine2} alt="Card Top" className="w-full h-auto" />
+
+        <div className="sketchfab-embed-wrapper"> <iframe title="3d Printable Air Balloon Engine for LEGO" allowFullScreen allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1879711af3664ef7bc150f90139e8d2a/embed?autospin=1&autostart=1" width="400" height="250"> </iframe> </div>
+
+
+  
+          {/* <img src={engine2} alt="Card Top" className="w-full h-auto" /> */}
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
             <p className="text-gray-700 text-base">
@@ -107,9 +103,10 @@ export const Features = () => {
           </div>
         </div>
 
-        {/* Second Card */}
+        {/* Third Card */}
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img  src={engine3} alt="Card Top" className="w-full h-auto" />
+        <div className="sketchfab-embed-wrapper"> <iframe title="Oscillating Cylinder Motor for LEGO (3d Print)" allowFullScreen allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/f66c6dc8059e4fa2b2a05c421cff81df/embed?autospin=1&autostart=1" width="400" height="250"> </iframe> </div>
+          {/* <img  src={engine3} alt="Card Top" className="w-full h-auto" /> */}
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
             <p className="text-gray-700 text-base">
@@ -122,6 +119,11 @@ export const Features = () => {
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
           </div>
         </div> 
+
+
+        
+      
+
       </div>
 
       
